353493d108652cef7e4ff523b50a95b20e90db61
Amritanshu Keshari <84897578+AnshuWorld@users.noreply.github.com>
2024-08-30 11:22:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function fun() {
+    var nav = document.getElementById('navbar');
+    var res = 'resp';
+    nav.classList.toggle(res);
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
